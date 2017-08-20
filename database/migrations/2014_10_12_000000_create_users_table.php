88d3da0b5caf1cf6c100197c2d8a3fd5216fe551
KamaiL\Admin <camel4882@gmail.com>
2017-08-20 20:44:50 +0700

    fixed db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email');
             $table->string('password');
+            $table->integer("point")->default(0);
             $table->rememberToken();
+            $table->enum('isAdmin',[0,1])->default("0");
             $table->timestamps();
         });
     }
